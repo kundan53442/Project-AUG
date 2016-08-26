bdf49a5ebc5519d321348ce9cfa0f0d461de55c4
kundan53442 <kundan53442@gmail.com>
2016-08-26 22:22:47 +0530

    added templates

--- js/frontpage.js
+++ js/frontpage.js
@@ -10,6 +10,10 @@ $(document).ready(function(){
       $("#step5").hide();
       $("#step6").hide();
       $("#front_page").show(500);
+      $("#template1").hide();
+      $("#template3").show();
+
+
 
       $("#f_button").on('click',function(){
         
@@ -22,6 +26,8 @@ $(document).ready(function(){
       	$("#step4").hide();
       	$("#step5").hide();
       	$("#step6").hide();
+      	$("#template1").hide();
+        $("#template2").hide();
 
       });
 
@@ -35,6 +41,8 @@ $(document).ready(function(){
       	$("#step4").hide();
       	$("#step5").hide();
       	$("#step6").hide();
+      	$("#template1").hide();
+      	$("#template2").hide();
 
       });
 
@@ -49,6 +57,8 @@ $(document).ready(function(){
       	$("#step4").hide(500);
       	$("#step5").hide(500);
       	$("#step6").hide(500);
+      	$("#template1").hide();
+      	$("#template2").hide();
 
       });
 
@@ -62,6 +72,8 @@ $(document).ready(function(){
       	$("#step4").hide();
       	$("#step5").hide();
       	$("#step6").hide();
+      	$("#template1").hide();
+      	$("#template2").hide();
 
       });
 
@@ -75,6 +87,8 @@ $(document).ready(function(){
       	$("#step4").hide();
       	$("#step5").hide();
       	$("#step6").hide();
+      	$("#template1").hide();
+      	$("#template2").hide();
 
       });
 
@@ -88,6 +102,8 @@ $(document).ready(function(){
       	$("#step4").show(500);
       	$("#step5").hide();
       	$("#step6").hide();
+      	$("#template1").hide();
+      	$("#template2").hide();
 
       });
 
@@ -101,6 +117,8 @@ $(document).ready(function(){
       	$("#step4").hide();
       	$("#step5").hide();
       	$("#step6").hide();
+      	$("#template1").hide();
+      	$("#template2").hide();
 
       });
 
@@ -114,6 +132,8 @@ $(document).ready(function(){
       	$("#step4").hide(500);
       	$("#step5").show(500);
       	$("#step6").hide();
+      	$("#template1").hide();
+      	$("#template2").hide();
 
       });
 
@@ -127,6 +147,8 @@ $(document).ready(function(){
       	$("#step4").hide(500);
       	$("#step5").hide();
       	$("#step6").hide();
+      	$("#template1").hide();
+      	$("#template2").hide();
 
       });
 
@@ -140,8 +162,11 @@ $(document).ready(function(){
       	$("#step4").hide();
       	$("#step5").hide(500);
       	$("#step6").show(500);
+      	$("#template1").hide();
+      	$("#template2").hide();
 
       });
+
 
       $("#s5_p").on('click',function(){
 
@@ -153,7 +178,86 @@ $(document).ready(function(){
       	$("#step4").show(500);
       	$("#step5").hide(500);
       	$("#step6").hide();
+        $("#template1").hide();
+      	$("#template2").hide();
+
+      });
+
+      $("#s6_p").on('click',function(){
+
+      	$("#header").show(500);
+      	$("#footer").show(500);
+      	$("#step1").hide();
+      	$("#step2").hide();
+      	$("#step3").hide();
+      	$("#step4").hide(500);
+      	$("#step5").show(500);
+      	$("#step6").hide();
+      	$("#template1").hide();
+      	$("#template2").hide();
+
+      });
+
+      $("#image1").on('click',function(){
+
+      	$("#header").hide(500);
+      	$("#footer").hide(500);
+      	$("#step1").hide();
+      	$("#step2").hide();
+      	$("#step3").hide();
+      	$("#step4").hide(500);
+      	$("#step5").hide(500);
+      	$("#step6").hide();
+      	$("#template1").show();
+      	$("#template2").hide();
+
+
+      });
+
+      $("#image2").on('click',function(){
+
+      	$("#header").hide(500);
+      	$("#footer").hide(500);
+      	$("#step1").hide();
+      	$("#step2").hide();
+      	$("#step3").hide();
+      	$("#step4").hide(500);
+      	$("#step5").hide(500);
+      	$("#step6").hide();
+      	$("#template1").hide();
+      	$("#template2").show();
+
 
       });
 });
+
+
+var user={
+
+	fullname                  :$("#usr_name").val(),
+	email                     :$("#usr_name").val(),
+	phone                     :$("usr_phone").val(),
+	specialization            :$("usr_specialization").val(),
+    experience                :$("usr_exp").val(),
+    job_description           :$("job_description").val(),
+    qual1                     :$("qualification1").val(),
+    college_name1             :$("college_name1").val(),
+    marks1                    :$("college_%1").val(),
+    sub1                      :$("college_sb1").val(),
+    qual1                     :$("qualification2").val(),
+    college_name1             :$("college_name2").val(),
+    marks1                    :$("college_%2").val(),
+    sub1                      :$("college_sb2").val(),
+    qual1                     :$("qualification3").val(),
+    college_name1             :$("college_name3").val(),
+    marks1                    :$("college_%3").val(),
+    sub1                      :$("college_sb3").val(),
+    extra_curricular          :$("extra_curricular").val(),
+    gender                    :$("gender").val(),
+    hobbie                    :$("hobbie").val(),
+    language                  :$("language" ).val(),
+    nationality               :$("nationality").val()
+};
+
+
 
