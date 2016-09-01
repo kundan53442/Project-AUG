0a3a41c0c5e792b62f714639e261453a2ee5f72a
kundan53442 <kundan53442@gmail.com>
2016-09-01 16:54:38 +0530

    add extra space for work exp and extra curricular activity

--- js/frontpage.js
+++ js/frontpage.js
@@ -37,7 +37,9 @@ $(document).ready(function(){
       	$("#header").show(500);
       	$("#footer").show(500);
       	$("#step1").hide(500);
-      	$("#step2").show(500);
+      	$("#step2").show(500); 
+                    $(".jd2").hide(); 
+                    $(".jd3").hide();
       	$("#step3").hide();
       	$("#step4").hide();
       	$("#step5").hide();
@@ -64,6 +66,43 @@ $(document).ready(function(){
 
       });
 
+      $(".add1").on('click',function(){
+
+        $("#header").show(500);
+        $("#footer").show(500);
+        $("#step1").hide(500);
+        $("#step2").show(500); 
+                    $(".jd1").show();
+                    $(".jd2").show(); 
+                    $(".jd3").hide();
+                    $(".add1").hide();
+        $("#step3").hide();
+        $("#step4").hide();
+        $("#step5").hide();
+        $("#step6").hide();
+        $("#template1").hide();
+        $("#thank_page").hide();
+      });
+
+      $(".add2").on('click',function(){
+
+        $("#header").show(500);
+        $("#footer").show(500);
+        $("#step1").hide(500);
+        $("#step2").show(500);
+                    $(".jd1").show();
+                    $(".jd2").show(); 
+                    $(".jd3").show();
+                    $(".add1").hide();
+                    $(".add2").hide();  
+        $("#step3").hide();
+        $("#step4").hide();
+        $("#step5").hide();
+        $("#step6").hide();
+        $("#template1").hide();
+        $("#thank_page").hide();
+      });
+
       
       $("#s2_n").on('click',function(){
 
@@ -79,6 +118,8 @@ $(document).ready(function(){
         $("#thank_page").hide();
 
       });
+
+
 
       $(".s2_p").on('click',function(){
 
@@ -104,9 +145,29 @@ $(document).ready(function(){
       	$("#step2").hide();
       	$("#step3").hide(500);
       	$("#step4").show(500);
+                $(".ac1").show();
+                $(".ac2").hide();
       	$("#step5").hide();
       	$("#step6").hide();
       	$("#template1").hide();
+        $("#thank_page").hide();
+
+      });
+
+      $(".add4").on('click',function(){
+
+        $("#header").show(500);
+        $("#footer").show(500);
+        $("#step1").hide();
+        $("#step2").hide();
+        $("#step3").hide(500);
+        $("#step4").show(500);
+                $(".ac1").show();
+                $(".ac2").show();
+                $(".add4").hide();
+        $("#step5").hide();
+        $("#step6").hide();
+        $("#template1").hide();
         $("#thank_page").hide();
 
       });
@@ -260,33 +321,51 @@ $(document).ready(function(){
       
 });
 
-// Data storing and retrieving 
+//******************************************* Data storing and retrieving 
 
 $(document).ready(function(){   
 
       $("form").on("submit",function(e){ e.preventDefault();
         
         var user={
-
+              // personal information storing
 	            fullname                  :$("#usr_name").val(),
 	            email                     :$("#usr_email").val(),
 	            phone                     :$("#usr_phone").val(),
 	            specialization            :$("#usr_specialization").val(),
-              experience                :$("#usr_exp").val(),
-              job_description           :$("#job_description").val(),
+              // Work experience details storing
+              employer1                 :$("#ce1").val(),
+              employee_from1            :$("#ce_f1").val(),
+              employee_to1              :$("#ce_t1").val(),
+              job_description1          :$("#job_description1").val(),
+              employer2                 :$("#pe2").val(),
+              employee_from2            :$("#pe_f2").val(),
+              employee_to2              :$("#pe_t2").val(),
+              job_description2          :$("#job_description2").val(),
+              employer3                 :$("#pe3").val(),
+              employee_from3            :$("#pe_f3").val(),
+              employee_to3              :$("#pe_t3").val(),
+              job_description3          :$("#job_description3").val(),
+              // Qualification details storing
               qual1                     :$("#qualification1").val(),
               college_name1             :$("#college_name1").val(),
               marks1                    :$("#college_p1").val(),
+              year1                     :$("#ay1").val(),
               sub1                      :$("#college_sb1").val(),
               qual2                     :$("#qualification2").val(),
               college_name2             :$("#college_name2").val(),
               marks2                    :$("#college_p2").val(),
+              year2                     :$("#ay2").val(),
               sub2                      :$("#college_sb2").val(),
               qual3                     :$("#qualification3").val(),
               college_name3             :$("#college_name3").val(),
               marks3                    :$("#college_p3").val(),
+              year3                     :$("#ay3").val(),
               sub3                      :$("#college_sb3").val(),
-              extra_curricular          :$("#extra_curricular").val(),
+              // Extra curricular information storing
+              extra_curricular1         :$("#extra_curricular1").val(),
+              extra_curricular2         :$("#extra_curricular2").val(),
+              //other information storing
               gender                    :$("#gender").val(),
               hobbie                    :$("#hobbie").val(),
               language                  :$("#language" ).val(),
@@ -294,27 +373,48 @@ $(document).ready(function(){
         }; 
 
         
-            // template 1
+// *************************Data storing and retrieving  for template 1
+
+               // personal information retrieving
            document.getElementById("r_name").innerHTML = user.fullname;
            document.getElementById("rd_name").innerHTML = user.fullname;
            document.getElementById("r_spl").innerHTML = user.specialization;
            document.getElementById("r_email").innerHTML = user.email;
            document.getElementById("r_phone").innerHTML = user.phone;
-           document.getElementById("r_n.exp").innerHTML = user.experience;
-           document.getElementById("r_j.des").innerHTML = user.job_description;
+               // Work experience details retrieving
+           document.getElementById("emp1").innerHTML = user.employer1;
+           document.getElementById("empfr1").innerHTML = user.employee_from1;
+           document.getElementById("empto1").innerHTML = user.employee_from2;
+           document.getElementById("r_j.des1").innerHTML = user.job_description1;
+           document.getElementById("emp2").innerHTML = user.employer2;
+           document.getElementById("empfr2").innerHTML = user.employee_from2;
+           document.getElementById("empto2").innerHTML = user.employee_to2;
+           document.getElementById("r_j.des2").innerHTML = user.job_description2;
+           document.getElementById("emp3").innerHTML = user.employer3;
+           document.getElementById("empfr3").innerHTML = user.employee_from3;
+           document.getElementById("empto3").innerHTML = user.employee_to3;
+           document.getElementById("r_j.des3").innerHTML = user.job_description3;
+               // Qualification details retrieving
            document.getElementById("stn1").innerHTML = user.qual1
            document.getElementById("college1").innerHTML = user.college_name1;
            document.getElementById("cgpa1").innerHTML = user.marks1;
+           document.getElementById("cgpa1").innerHTML = user.marks1;
+           document.getElementById("ayr1").innerHTML = user.year1;
            document.getElementById("sub1").innerHTML = user.sub1;
            document.getElementById("stn2").innerHTML = user.qual2
            document.getElementById("college2").innerHTML = user.college_name2;
            document.getElementById("cgpa2").innerHTML = user.marks2;
+           document.getElementById("ayr2").innerHTML = user.year2;
            document.getElementById("sub2").innerHTML = user.sub2;
            document.getElementById("stn3").innerHTML = user.qual3
            document.getElementById("college3").innerHTML = user.college_name3;
            document.getElementById("cgpa3").innerHTML = user.marks3;
+           document.getElementById("ayr3").innerHTML = user.year3;
            document.getElementById("sub3").innerHTML = user.sub3;
-           document.getElementById("r_EC").innerHTML = user.extra_curricular;
+               // Extra curricular information retrieving
+           document.getElementById("r_EC1").innerHTML = user.extra_curricular1;
+           document.getElementById("r_EC2").innerHTML = user.extra_curricular2;
+               // other information retrieving
            document.getElementById("r_gender").innerHTML = user.gender;
            document.getElementById("r_hobbies").innerHTML = user.hobbie;
            document.getElementById("r_lan").innerHTML = user.language;
@@ -322,27 +422,47 @@ $(document).ready(function(){
 
 
 
-           //template2
+//*********************************Data storing and retrieving for template2
+
+                // personal information retrieving
            document.getElementById("2r_name").innerHTML = user.fullname;
            document.getElementById("2rd_name").innerHTML = user.fullname;
            document.getElementById("2r_spl").innerHTML = user.specialization;
            document.getElementById("2r_email").innerHTML = user.email;
            document.getElementById("2r_phone").innerHTML = user.phone;
-           document.getElementById("2r_n.exp").innerHTML = user.experience;
-           document.getElementById("2r_j.des").innerHTML = user.job_description;
+               // Work experience details retrieving
+           document.getElementById("2emp1").innerHTML = user.employer1;
+           document.getElementById("2empfr1").innerHTML = user.employee_from1;
+           document.getElementById("2empto1").innerHTML = user.employee_from2;
+           document.getElementById("2r_j.des1").innerHTML = user.job_description1;
+           document.getElementById("2emp2").innerHTML = user.employer2;
+           document.getElementById("2empfr2").innerHTML = user.employee_from2;
+           document.getElementById("2empto2").innerHTML = user.employee_to2;
+           document.getElementById("2r_j.des2").innerHTML = user.job_description2;
+           document.getElementById("2emp3").innerHTML = user.employer3;
+           document.getElementById("2empfr3").innerHTML = user.employee_from3;
+           document.getElementById("2empto3").innerHTML = user.employee_to3;
+           document.getElementById("2r_j.des3").innerHTML = user.job_description3;
+               // Qualification details retrieving
            document.getElementById("2stn1").innerHTML = user.qual1
            document.getElementById("2college1").innerHTML = user.college_name1;
            document.getElementById("2cgpa1").innerHTML = user.marks1;
-           document.getElementById("2sub1").innerHTML = user.sub1;
+           document.getElementById("2ayr1").innerHTML = user.year1;
+           document.getElementById("2sub1").innerHTML = user.sub1;    
            document.getElementById("2stn2").innerHTML = user.qual2
            document.getElementById("2college2").innerHTML = user.college_name2;
            document.getElementById("2cgpa2").innerHTML = user.marks2;
+           document.getElementById("2ayr2").innerHTML = user.year2;
            document.getElementById("2sub2").innerHTML = user.sub2;
            document.getElementById("2stn3").innerHTML = user.qual3
            document.getElementById("2college3").innerHTML = user.college_name3;
            document.getElementById("2cgpa3").innerHTML = user.marks3;
+           document.getElementById("2ayr3").innerHTML = user.year3;
            document.getElementById("2sub3").innerHTML = user.sub3;
-           document.getElementById("2r_EC").innerHTML = user.extra_curricular;
+               // Extra curricular information retrieving
+           document.getElementById("2r_EC1").innerHTML = user.extra_curricular1;
+           document.getElementById("2r_EC2").innerHTML = user.extra_curricular2;
+               // other information retrieving
            document.getElementById("2r_gender").innerHTML = user.gender;
            document.getElementById("2r_hobbies").innerHTML = user.hobbie;
            document.getElementById("2r_lan").innerHTML = user.language;
@@ -355,10 +475,6 @@ $(document).ready(function(){
       // function to print page    
            function printPage() {
              window.print();
-           };
-      // Elert for next button click
-           function alert(){
-             alert("Please submit this step")
            };
 
 
